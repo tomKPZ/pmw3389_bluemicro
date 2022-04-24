ac3d6e5b216008973f3f0b86b50d61d83c287eae
tomKPZ <tomKPZ@gmail.com>
2022-04-24 10:47:49 -0700

    BLE HID

--- pmw3389_bluemicro.ino
+++ pmw3389_bluemicro.ino
@@ -16,13 +16,62 @@ void setup() {
   pinMode(12, OUTPUT);
   digitalWrite(12, HIGH);
 
-  Serial.begin(9600);
-  while (!Serial) {}
+  Serial.begin(115200);
+  while (!Serial) {
+  }
 
   if (sensor.begin(SS, 16000)) // 10 is the pin connected to SS of the module.
     Serial.println("Sensor initialization successed");
   else
     Serial.println("Sensor initialization failed");
+
+  // init_bluetooth();
+}
+
+void init_bluetooth() {
+  Bluefruit.begin();
+  // HID Device can have a min connection interval of 9*1.25 = 11.25 ms
+  Bluefruit.Periph.setConnInterval(
+      9, 16);              // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
+  Bluefruit.setTxPower(4); // Check bluefruit.h for supported values
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather 52");
+  bledis.begin();
+
+  // BLE HID
+  blehid.begin();
+
+  // Set up and start advertising
+  startAdv();
+}
+
+void startAdv() {
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_MOUSE);
+
+  // Include BLE HID service
+  Bluefruit.Advertising.addService(blehid);
+
+  // There is enough room for 'Name' in the advertising packet
+  Bluefruit.Advertising.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   *
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244); // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);   // number of seconds in fast mode
+  Bluefruit.Advertising.start(0); // 0 = Don't stop advertising after n seconds
 }
 
 void loop() {
@@ -33,6 +82,8 @@ void loop() {
     Serial.print("\t");
     Serial.print(data.dy);
     Serial.println();
+
+    blehid.mouseMove(data.dx, data.dy);
   }
 
   delay(10);
